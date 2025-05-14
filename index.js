1162725f3f36b3d24c3f2e78a4ac2a8e452c4bfa
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-05-14 16:47:12 -0500

    Pokemons

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-import { pokemon } from "./fetch.js";
+import { searchPokemons } from "./fetch.js";
+const pokemons = document.getElementById("pokemons");
+const {results} = await searchPokemons()
+// console.log(results);
 
-console.log( await pokemon())
+const extractPokemon = async (url) => {
+  const res = await fetch(url)
+  const data = await res.json()
+  console.log(data.sprites.front_default);
+  insertDom(data.sprites.front_default)
+}
+
+results.forEach( result =>  extractPokemon(result.url) );
+
+const insertDom = (pokemonUrl) => {
+  const cardPokemon = `
+    <div class="pokemon">
+      <img src="${pokemonUrl}"
+    </div>
+  `
+pokemons.insertAdjacentHTML("beforeend", cardPokemon);
+}

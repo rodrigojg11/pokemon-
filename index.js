ec50688cbfbf993386adc06531fcca127e10f177
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-05-14 16:52:08 -0500

    Pokemons

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const extractPokemon = async (url) => {
   const res = await fetch(url)
   const data = await res.json()
   console.log(data.sprites.front_default);
-  insertDom(data.sprites.front_default)
+  const {sprites: {front_default}} = data;
+  insertDom(front_default)
 }
 
 results.forEach( result =>  extractPokemon(result.url) );
 
 const insertDom = (pokemonUrl) => {
   const cardPokemon = `
-    <div class="pokemon">
+    <div class="pokemon col-4">
       <img src="${pokemonUrl}"
     </div>
   `
